1461500e82a8f3b9cf197971a93c47958ada6cf7
Ken Mueller <kenmueller0@gmail.com>
2021-11-06 19:14:43 -0700

    initial commit

---
+++ routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>appzone</title>
+</svelte:head>
+
+<main>
+	<h1>appzone</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		height: 100%;
+	}
+
+	h1 {
+		margin: auto;
+	}
+</style>
